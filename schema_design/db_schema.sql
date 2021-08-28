cde993b072c2a6e0ae55c5c5b1fa3a0f40e6459b
Sergey Simenshteyn <simenshteyn@MacBook-Air-Sergey.local>
2021-08-28 18:38:50 +0700

    Fully working db_schema.sql

--- schema_design/db_schema.sql
+++ schema_design/db_schema.sql
@@ -13,8 +13,8 @@ CREATE TABLE IF NOT EXISTS content.movies (
     movie_desc      text,
     movie_rating    numeric(2, 1)
                     CHECK (movie_rating BETWEEN 0 AND 10),
-    created_at      datetime    DEFAULT (now()),
-    updated_at      datetime,
+    created_at      timestamp    DEFAULT (now()),
+    updated_at      timestamp,
     UNIQUE (movie_title, movie_desc, movie_rating)
 );
 
@@ -23,8 +23,8 @@ CREATE TABLE IF NOT EXISTS content.people (
     full_name       text        NOT NULL,
     person_desc     text,
     birthday        date,
-    created_at      datetime    DEFAULT (now()),
-    updated_at      datetime,
+    created_at      timestamp    DEFAULT (now()),
+    updated_at      timestamp,
     UNIQUE (full_name, birthday)
 );
 
@@ -32,8 +32,8 @@ CREATE TABLE IF NOT EXISTS content.genres (
     genre_id        uuid        PRIMARY KEY,
     genre_name      text        UNIQUE NOT NULL,
     genre_desc      text,
-    created_at      datetime    DEFAULT (now()),
-    updated_at      datetime,
+    created_at      timestamp   DEFAULT (now()),
+    updated_at      timestamp
 );
 
 CREATE TABLE IF NOT EXISTS content.movie_people (
