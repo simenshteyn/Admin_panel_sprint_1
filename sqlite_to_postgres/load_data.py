a41eeb04cef22c0298bdcd57657400d483988d9b
Sergey Simenshteyn <simenshteyn@MacBook-Air-Sergey.local>
2021-08-29 11:45:12 +0700

    Draft of load_data.py

--- sqlite_to_postgres/load_data.py
+++ sqlite_to_postgres/load_data.py
@@ -15,6 +15,11 @@ def load_from_sqlite(connection: sqlite3.Connection, pg_conn: _connection):
 
 
 if __name__ == '__main__':
-    dsl = {'dbname': 'movies_database', 'user': 'postgres', 'password': 1234, 'host': '127.0.0.1', 'port': 5432}
+    dsl = {'dbname': 'movies_database',
+           'user': 'postgres',
+           'password': 1234,
+           'host': '127.0.0.1',
+           'port': 5432
+           }
     with sqlite3.connect('db.sqlite') as sqlite_conn, psycopg2.connect(**dsl, cursor_factory=DictCursor) as pg_conn:
         load_from_sqlite(sqlite_conn, pg_conn)
