1d2d6362c9a17e57ab050a323a47a639805a6b88
Sergey Simenshteyn <simenshteyn@MacBook-Air-Sergey.local>
2021-09-02 16:44:23 +0700

    before m:n relations

--- movies_admin/movies/admin.py
+++ movies_admin/movies/admin.py
@@ -2,17 +2,22 @@ from django.contrib import admin
 
 from .models import Movies, People, Genres, MoviePeople, MovieGenres
 
-class MovieGenresInline(admin.StackedInline):
+class MovieGenresInline(admin.TabularInline):
     model = MovieGenres
+    extra = 0
+    show_change_link = True
 
 
-class MoviePeopleInline(admin.StackedInline):
+class MoviePeopleInline(admin.TabularInline):
     model = MoviePeople
+    extra = 0
+    show_change_link = True
 
 @admin.register(Genres)
 class GenresAdmin(admin.ModelAdmin):
     search_fields = ['genre_name']
     ordering = ['genre_name']
+    list_display = ['genre_name']
 
 
 @admin.register(People)
