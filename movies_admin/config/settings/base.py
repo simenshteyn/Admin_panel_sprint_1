dc3cc1c1dc9b51993a11d7fe31f6efd1c1ef7c56
Sergey Simenshteyn <simenshteyn@MacBook-Air-Sergey.local>
2021-09-11 13:04:25 +0700

    debug toolbar added

--- movies_admin/config/settings/base.py
+++ movies_admin/config/settings/base.py
@@ -16,6 +16,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'debug_toolbar',
+    'django_extensions',
     'movies',
 ]
 
@@ -27,6 +29,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'config.urls'
